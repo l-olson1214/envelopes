12e874b5bcb235b3d050dd750e2ec911e1afbdf8
Lindsey Olson <l.olson.swift@gmail.com>
2023-08-30 13:46:01 -0400

    Initial Commit

---
+++ Envelopes/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Envelopes
+//
+//  Created by Lindsey Olson on 8/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Envelopes/EnvelopesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvelopesApp.swift
+//  Envelopes
+//
+//  Created by Lindsey Olson on 8/30/23.
+//
+
+import SwiftUI
+
+@main
+struct EnvelopesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
